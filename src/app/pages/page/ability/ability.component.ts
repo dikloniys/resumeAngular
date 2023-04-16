90925d39d91c127f9c97f8b8950508d8a8a988b0
dikloniys <vavan01099400@yandex.ru>
2023-04-16 15:12:23 +0300

    made changes on the abilities page

--- src/app/pages/page/ability/ability.component.ts
+++ src/app/pages/page/ability/ability.component.ts
@@ -6,5 +6,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./ability.component.sass']
 })
 export class AbilityComponent {
-
+  abilities=[
+    {
+      screen:'/assets/imgs/abilit/github.png',
+      abilityName:'github',
+      description:'',
+    },
+    {
+      screen:'/assets/imgs/abilit/bem.png',
+      abilityName:'bem',
+      description:'',
+    },
+    {
+      screen:'/assets/imgs/abilit/sass.png',
+      abilityName:'sass',
+      description:'',
+    },
+    {
+      screen:'/assets/imgs/abilit/photoshop.png',
+      abilityName:'photoshop',
+      description:'',
+    },
+    {
+      screen:'/assets/imgs/abilit/figma.png',
+      abilityName:'figma',
+      description:'',
+    },
+    {
+      screen:'/assets/imgs/abilit/jquery.png',
+      abilityName:'jquery',
+      description:'',
+    },
+    {
+      screen:'/assets/imgs/abilit/angular.png',
+      abilityName:'angular',
+      description:'',
+    },
+  ]
 }
